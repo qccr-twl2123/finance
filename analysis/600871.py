e165083324bb978494315737dccc408856c316d6
mark1xie <545739504@qq.com>
2017-10-06 16:35:21 +0800

    add

--- analysis/600871.py
+++ analysis/600871.py
@@ -3,10 +3,19 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
-import sys
 
 plt.rcParams['font.sans-serif']=['SimHei'] #用来正常显示中文标签
 plt.rcParams['axes.unicode_minus']=False #用来正常显示负号
 
-df = pd.read_csv("600871.csv")
-print df
+df=pd.read_csv("600871.csv")
+
+# pd.DataFrame(columns=['open','close'])
+#
+# open = df["open"].T.values
+# close = df["close"].T.values
+# df['open'] = open
+# df['close'] = close
+#
+df.plot()
+plt.show()
+
