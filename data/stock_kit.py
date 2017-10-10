05dc125eabca2869392a5624352a7fd58d923964
mark1xie <545739504@qq.com>
2017-10-10 20:46:32 +0800

    add

--- data/stock_kit.py
+++ data/stock_kit.py
@@ -2,12 +2,18 @@
 # -*- coding: UTF-8 -*-
 
 import tushare as ts
+import matplotlib.pyplot as plt
 
 def get_hist_data(code):
     return ts.get_hist_data(code)
 
-def get_hist_data_plot():
-    return None
+
+def show_stock_figure(df):
+    x = df.index
+    y = df["close"].T.values
+    plt.plot(x,y)
+    plt.show()
+
 
 if __name__=="__main__":
-    print  get_hist_data("600871")
+    print get_hist_data("002148")
