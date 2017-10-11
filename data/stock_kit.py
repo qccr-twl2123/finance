ef15e0e839e644ba30cab744d26fd5e2ca8ca993
mark1xie <545739504@qq.com>
2017-10-11 17:58:26 +0800

    stock basic

--- data/stock_kit.py
+++ data/stock_kit.py
@@ -3,17 +3,30 @@
 
 import tushare as ts
 import matplotlib.pyplot as plt
+import pandas as pd
+import matplotlib.dates as mdates
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 def get_hist_data(code):
-    return ts.get_hist_data(code)
+    df = ts.get_stock_basics()
+    return  df.ix[code]['name'],ts.get_hist_data(code)
 
 
-def show_stock_figure(df):
-    x = df.index
+def show_stock_figure(name,df):
+    x = pd.to_datetime(df.index,format="%Y-%m-%d")
     y = df["close"].T.values
-    plt.plot(x,y)
+
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+    plt.plot(x,y,c='red')
+    plt.xlabel("交易日期")
+    plt.ylabel("收盘价")
+    plt.title(name,loc="right")
+    plt.gcf().autofmt_xdate()
     plt.show()
 
 
 if __name__=="__main__":
-    print get_hist_data("002148")
+     name,bwkj = get_hist_data("002148")
+     show_stock_figure(name,bwkj[bwkj.index>'2017-08-25'])
