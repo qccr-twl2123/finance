09a05d62f1f03cd20a561ba028617924daae7ea3
mark1xie <545739504@qq.com>
2017-11-11 16:51:02 +0800

    add

--- util/RMB_Supply.py
+++ util/RMB_Supply.py
@@ -4,17 +4,19 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import matplotlib.dates as mdates
 import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
 class RmbSupply(object):
 
-    def __init__(self,year):
-        self.__year = year
+    def __init__(self,year_list):
+        self.__year_list = year_list
 
-    def generate_analysis_chart(self):
-        df = pd.read_csv(self.__year+"_RMB_SUPPLY.csv",index_col="Item")
+    def covert_to_data_frame(self, year):
+
+        df = pd.read_csv(year+"_RMB_SUPPLY.csv",index_col="Item")
         dates = pd.to_datetime(df.T.index)
         m0_1 = df.T["M0"].values
         m1_1 = df.T["M1"].values
@@ -28,10 +30,39 @@ class RmbSupply(object):
         m_pd['m0'] = m0
         m_pd['m1'] = m1
         m_pd['m2'] = m2
-        print m_pd
+        return m_pd
+
+    def contact_data(self):
+        year_data_list = []
+        for year in self.__year_list:
+            year_data_list.append(self.covert_to_data_frame(year))
+
+        df = pd.concat(year_data_list,ignore_index=False)
+        return df
+
+    def calculate_growth_rate(self):
+        df = self.contact_data()
+        df["m0增长率"] = (df["m0"] - df["m0"].shift(-1))/df["m0"]
+        df["m1增长率"] = (df["m1"] - df["m1"].shift(-1))/df["m1"]
+        df["m2增长率"] = (df["m2"] - df["m2"].shift(-1))/df["m2"]
+        return df
+
+    def generate_analysis_chart(self):
+        df = self.calculate_growth_rate()
+        print df
+        m0 = df["m0增长率"]
+        m1 = df["m1增长率"]
+        m2 = df["m2增长率"]
+        plt.scatter(m0,m2,color="r")
+        plt.show()
+
 
 
 
 if __name__=="__main__":
-     rmb_supply = RmbSupply("2017")
-     rmb_supply.generate_analysis_chart();
+     year_list = ["2015","2016","2017"]
+     rmb_supply = RmbSupply(year_list)
+     # print rmb_supply.contact_data()
+     # rmb_supply.calculate_growth_rate()
+     rmb_supply.generate_analysis_chart()
+
