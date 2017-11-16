149732750e5f3cf7a0b9523739e14e35ec0d879b
mark1xie <545739504@qq.com>
2017-11-16 15:00:13 +0800

    add

--- util/Splider.py
+++ util/Splider.py
@@ -4,13 +4,19 @@ import urllib
 import urllib2
 import re
 import requests
+from bs4 import BeautifulSoup
 
 headers = {
-    'Cookie': '_gscu_1042262807=972428447ozpcs69; ccpassport=22bce01f02221818e5251f4365225439; wzwsconfirm=89349685405c82c4de5a57ca4b6bb86c; wzwsvtime=1510459197; wzwstemplate=NA==; wzwschallenge=V1pXU19DT05GSVJNX1BSRUZJWF9MQUJFTDQxMzEwODc=',
+    'Cookie': '_gscu_1042262807=972428447ozpcs69; ccpassport=d277e785b88a56eb0e72f0da2a55f88a; wzwsconfirm=66a5c565b5e63b8276f26d51130cee96; wzwsvtime=1510725850; wzwstemplate=MQ==; wzwschallenge=V1pXU19DT05GSVJNX1BSRUZJWF9MQUJFTDExMTY1MjM=',
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
 }
 
 url = 'http://www.pbc.gov.cn/zhengcehuobisi/125207/125213/125440/125838/125888/2968982/index.html'
 r = requests.get(url, headers=headers)
 r.encoding = 'utf'
-print r.text
+soup = BeautifulSoup(r.text,"lxml")
+body = soup.find_all("tbody")[0]
+print body
+item_list = body.find_all("tr",attrs={"style","height:15pt"})
+print item_list
+
