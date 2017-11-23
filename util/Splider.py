497d8450455a8ae996d3595e232b29224fbb256f
mark1xie <545739504@qq.com>
2017-11-23 13:54:44 +0800

    2014货币供应量爬取

--- util/Splider.py
+++ util/Splider.py
@@ -13,7 +13,7 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
 }
 
-url = 'http://www.pbc.gov.cn/eportal/fileDir/defaultCurSite/resource/cms/2015/07/2014s07.htm'
+url = 'http://www.pbc.gov.cn/eportal/fileDir/defaultCurSite/resource/cms/2015/07/2013s07.htm'
 r = requests.get(url, headers=headers)
 r.encoding = 'utf'
 soup = BeautifulSoup(r.text,"lxml")
@@ -24,24 +24,29 @@ year_list = [datetime.strptime(x,'%Y-%m-%d') for x in year_list]
 
 tr = soup.find_all("tr",attrs={"style":"mso-height-source:userset;height:19.5pt","height":"26"})
 
-m2_list = map(lambda x: str(x.string).strip(), soup.find_all("td", class_="xl55"))
-m2_list = m2_list + map(lambda x: str(x.string).strip(), soup.find_all("td", class_="xl57"))
-m2_list = m2_list + map(lambda x: str(x.string).strip(), soup.find_all("td", class_="xl59"))
+m2_list = map(lambda x: str(x.string).strip(), soup.find_all("td", class_="xl52"))
+m2_list = m2_list + map(lambda x: str(x.string).strip(), soup.find_all("td", class_="xl54"))
+m2_list = m2_list + map(lambda x: str(x.string).strip(), soup.find_all("td", class_="xl56"))
 
-m1_list = map(lambda x: str(x.string).strip(), tr[2].find_all("td", class_="xl63"))
-m1_list = m1_list + map(lambda x: str(x.string).strip(), tr[2].find_all("td", class_="xl64"))
-m1_list = m1_list + map(lambda x: str(x.string).strip(), tr[2].find_all("td", class_="xl65"))
+m1_list = map(lambda x: str(x.string).strip(), tr[2].find_all("td", class_="xl60"))
+m1_list = m1_list + map(lambda x: str(x.string).strip(), tr[2].find_all("td", class_="xl61"))
+m1_list = m1_list + map(lambda x: str(x.string).strip(), tr[2].find_all("td", class_="xl62"))
 
-m0_list = map(lambda x: str(x.string).strip(), tr[4].find_all("td", class_="xl66"))
-m0_list = m0_list + map(lambda x: str(x.string).strip(), tr[4].find_all("td", class_="xl64"))
-m0_list = m0_list + map(lambda x: str(x.string).strip(), tr[4].find_all("td", class_="xl65"))
+m0_list = map(lambda x: str(x.string).strip(), tr[4].find_all("td", class_="xl63"))
+m0_list = m0_list + map(lambda x: str(x.string).strip(), tr[4].find_all("td", class_="xl61"))
+m0_list = m0_list + map(lambda x: str(x.string).strip(), tr[4].find_all("td", class_="xl62"))
+
+print year_list.__len__()
+print m2_list.__len__()
+print m1_list.__len__()
+print m0_list.__len__()
 
 df = pd.DataFrame(index=year_list,columns=["M2","M1","M0"])
 df['M2'] = m2_list
 df['M1'] = m1_list
 df['M0'] = m0_list
 print df
-df.to_csv("2014.csv")
+df.to_csv("2013_RMB_SUPPLY.csv")
 
 
 
