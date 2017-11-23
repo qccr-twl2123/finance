6ed46ecef0bc6dd0ece72c51058a41f39ad7e7c5
mark1xie <545739504@qq.com>
2017-11-23 13:46:45 +0800

    2014货币供应量爬取

--- util/Splider.py
+++ util/Splider.py
@@ -5,8 +5,8 @@ import re
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-
-
+import tushare as ts
+from  datetime  import  *
 
 headers = {
     'Cookie': '_gscu_1042262807=972428447ozpcs69; ccpassport=d277e785b88a56eb0e72f0da2a55f88a; wzwsconfirm=f44934d3368ed61012361b0fd08d400d; wzwsvtime=1511244744; wzwstemplate=OA==; wzwschallenge=V1pXU19DT05GSVJNX1BSRUZJWF9MQUJFTDgxNTI0NjU=',
@@ -18,24 +18,30 @@ r = requests.get(url, headers=headers)
 r.encoding = 'utf'
 soup = BeautifulSoup(r.text,"lxml")
 td_title = soup.find_all("td", class_="xl29")
-year_list = map(lambda x: str(x.string).strip(), td_title)
+year_list = map(lambda x: str(x.string).strip().replace(".","-")+"-01", td_title)
+year_list.append("2014-12-01")
+year_list = [datetime.strptime(x,'%Y-%m-%d') for x in year_list]
 
 tr = soup.find_all("tr",attrs={"style":"mso-height-source:userset;height:19.5pt","height":"26"})
 
-m2_list = map(lambda x: str(x.string).strip(), soup.find_all("td", class_="xl57"))
+m2_list = map(lambda x: str(x.string).strip(), soup.find_all("td", class_="xl55"))
+m2_list = m2_list + map(lambda x: str(x.string).strip(), soup.find_all("td", class_="xl57"))
 m2_list = m2_list + map(lambda x: str(x.string).strip(), soup.find_all("td", class_="xl59"))
 
-m1_list = map(lambda x: str(x.string).strip(), tr[2].find_all("td", class_="xl64"))
-m1_list = m1_list+ map(lambda x: str(x.string).strip(), tr[2].find_all("td", class_="xl65"))
+m1_list = map(lambda x: str(x.string).strip(), tr[2].find_all("td", class_="xl63"))
+m1_list = m1_list + map(lambda x: str(x.string).strip(), tr[2].find_all("td", class_="xl64"))
+m1_list = m1_list + map(lambda x: str(x.string).strip(), tr[2].find_all("td", class_="xl65"))
 
 m0_list = map(lambda x: str(x.string).strip(), tr[4].find_all("td", class_="xl66"))
 m0_list = m0_list + map(lambda x: str(x.string).strip(), tr[4].find_all("td", class_="xl64"))
 m0_list = m0_list + map(lambda x: str(x.string).strip(), tr[4].find_all("td", class_="xl65"))
-print year_list
-print m2_list
-print m1_list
-print m0_list
 
+df = pd.DataFrame(index=year_list,columns=["M2","M1","M0"])
+df['M2'] = m2_list
+df['M1'] = m1_list
+df['M0'] = m0_list
+print df
+df.to_csv("2014.csv")
 
 
 
