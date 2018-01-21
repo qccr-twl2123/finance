41db8019b4b2ee5481aa29327e4d48850c34a6b3
mark1xie <545739504@qq.com>
2018-01-21 20:26:32 +0800

    财报

--- util/company.py
+++ util/company.py
@@ -22,7 +22,7 @@ class Company(object):
     def bar(self):
         fig = plt.figure(1)
         ax1 = plt.subplot(111)
-        df = company.convert_csv_to_pd()
+        df = self.convert_csv_to_pd()
         data = (df['stock'].T.values).tolist()
         width = 0.5
         x_bar = np.arange(len(data))
@@ -38,7 +38,8 @@ class Company(object):
         ax1.set_ylabel("sales")
         ax1.set_title("stock")
         ax1.grid(True)
-        ax1.set_ylim(min(data)-10000, max(data)+10000)
+        ax1.set_ylim(min(data)-100000, max(data)+10000)
+        print min(data)-100000
         print xtick_list
         print data
         print min(data)
@@ -46,12 +47,33 @@ class Company(object):
         plt.xticks(rotation="89")
         plt.show()
 
+    def financial_analysis(object, year_list, code):
+        net_profits_list = []
+        index_list = []
+        report_date_list = []
+        for year in year_list:
+            for m in range(3):
+                index_list.append(str(year)+"-"+str(m+1))
+                report_data = ts.get_report_data(year, m+1)
+                report_data = report_data.set_index("code")
+                report_data = report_data.ix[code]
+                report_date_list.append(str(year)+report_data['report_date'])
+                net_profits_list.append(report_data['report_date','net_profits'])
+
+        summary = pd.DataFrame(index=index_list, columns=(['report_date', 'net_profits']))
+        summary['net_profits'] = net_profits_list
+        summary['report_date'] = report_date_list
+        summary.to_csv(code+"_finance.csv",index=index_list,sep=',')
 
 
 
 if __name__ == '__main__':
-       df = ts.get_cashflow_data(2014,3)
+       # df = ts.get_report_data(2017,3)
+       # df = df.set_index("code")
+       # xs  = df.ix['600231']
+       # print xs
+       # print  '2017-'+xs['report_date']
+       company =  Company("600688.csv")
+       year_list = [2017]
+       company.financial_analysis(year_list, '600231')
 
-       print df.T
-       # company =  Company("600688.csv")
-       # company.bar()
